f62a524d91976e2f943ce9c94b017b9dc7752a34
AarthiNataraj <gogulaarthin@neophrontech.com>
2020-07-01 18:03:48 +0530

    statemanagement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { ChildComponent } from './child/child.component';
 import { ShareService } from './share.service';
+import { StateService } from './state.service';
+
 
 
 @Component({
@@ -15,13 +17,19 @@ export class AppComponent implements AfterViewInit {
   @ViewChild(ChildComponent, {static: false}) child;
   //child is a user defined variable
 
-  constructor(private shareService: ShareService) { }
+  constructor
+    (private shareService: ShareService,
+    private state:StateService
+    ) { }
 
   ngAfterViewInit() {
     // parent <=> child communication
-    this.shareService.currentMessage.subscribe((data) => {
-      console.log('Parent Share Message', data);
-    });
+    // this.shareService.currentMessage.subscribe((data) => {
+    // console.log('Parent Share Message', data);
+
+    // });
+    this.state.set(this.state.data.username, 'Aarthi');
+
    // this.childMessage = this.child.childtoparent;
     //console.log(this.child.childtoparent);
 
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ShareService } from '../share.service';
+import { StateService } from '../state.service';
 
 
 @Component({
@@ -11,11 +12,15 @@ export class ChildComponent implements OnInit {
   @Input() childMessage: any;
   @Output() messageEvent = new EventEmitter<string>();
   public childtoparent = 'I am from child component';
-  constructor(private shareService: ShareService) { }
+  constructor
+  (private shareService: ShareService,
+   private state: StateService) { }
   clickFunction()
   {
     //this.messageEvent.emit('Message Emitted From Child');
-    this.shareService.changeMessage('Button Clicked So Trigger Emission');
+    //this.shareService.changeMessage('Button Clicked So Trigger Emission');
+    console.log(this.state.get(this.state.data.username));
+
   }
   ngOnInit()  {
       //console.log(this.childMessage);
